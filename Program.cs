75ed5b2e8350b8478b1a8cb00bdd3ee23b0503b8
Tetiana Dudarenko <tanya.dudarenko@gmail.com>
2020-07-31 22:54:16 +0300

    Initial setup

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace delegates_repeat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
