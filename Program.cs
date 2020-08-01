8f83c86d429a86115f741be74b41f6312cc9d94f
Tetiana Dudarenko <tanya.dudarenko@gmail.com>
2020-08-01 13:35:57 +0300

    Added event handlers and subcribed to them

--- Program.cs
+++ Program.cs
@@ -6,13 +6,35 @@ namespace delegates_repeat
     {
         static void Main(string[] args)
         {
-            int minutes, seconds = 0;
-            Console.WriteLine("Enter minutes:");
-            if(Int32.TryParse(Console.ReadLine(), out minutes)) {
-                Console.WriteLine("Enter seconds:");
-                if(Int32.TryParse(Console.ReadLine(), out seconds)){}
-            }
-            var timer = new TimerModel(minutes, seconds);
+            //User input
+            int seconds = 0;
+            Console.WriteLine("Enter seconds:");
+            if(Int32.TryParse(Console.ReadLine(), out seconds)) {}
+
+            Console.WriteLine("Enter a message to transmit:");
+            var timer = new TimerModel(seconds, Console.ReadLine());
+            var timerObserver = new TimerObserver(timer);
+            timerObserver.SubscribeToTimerEvents();
+
+            Console.WriteLine("Timer count down: ");
+            timer.StartCountDown();
+        }
+    }
+
+/// <summary>
+/// Class that transmits timer data to the console
+/// </summary>
+    public class TimerObserver {
+        private TimerModel timer;
+        public TimerObserver(TimerModel timer)
+        {
+            this.timer = timer;
+        }
+
+        public void SubscribeToTimerEvents()
+        {
+            timer.TimerElapsed += (sender, obj) => { Console.Write("\n" + obj.MessageFromTimer); };
+            timer.TimerChanged += (sender, obj) => { Console.Write($"\r{ obj.TimerCurrentTimer }  "); };
         }
     }
 }
--- TimerModel.cs
+++ TimerModel.cs
@@ -1,16 +1,69 @@
 using System;
+using System.Threading;
 
 namespace delegates_repeat
 {
+    /// <summary>
+    /// Class that describes the timer instance
+    /// Contains two fields: seconds and message to transmit
+    /// seconds: corresponds for seconds for timer countdown
+    /// message to transmit: corresponds for message that should be transmitted after 
+    /// the timer elapsed
+    /// </summary>
     public class TimerModel
     {
-        public int Minutes { get; set; }
-        public int Seconds { get; set; }
+        private int seconds;
+        private string messageToTransmit;
 
-        public TimerModel(int min, int sec)
-        {
-            Minutes = min;
-            Seconds = sec;
+        public TimerModel(int sec, string message) {
+            this.seconds = sec;
+            this.messageToTransmit = message;
         }
+
+        /// <summary>
+        /// Methods that starts the count down and raises events of its change and end
+        /// </summary>
+        public void StartCountDown() {
+            OnTimerChanged(new TimerChangedEventArgs() { TimerCurrentTimer = seconds });
+            while(seconds > 0)
+            {
+                WaitForOneSecond();
+                seconds -= 1;
+                OnTimerChanged(new TimerChangedEventArgs() { TimerCurrentTimer = seconds });
+            }
+            OnTimerElapsed(new TimerElapsedEventArgs(){ MessageFromTimer = messageToTransmit});
+        }
+
+        private void WaitForOneSecond() {
+            Thread.Sleep(1000);
+        }
+
+        /// <summary>
+        /// Event that is fired when the value of timer has changed
+        /// </summary>
+        public event EventHandler<TimerChangedEventArgs> TimerChanged;
+
+        protected virtual void OnTimerChanged(TimerChangedEventArgs e) {
+            EventHandler<TimerChangedEventArgs> handler = TimerChanged;
+            handler?.Invoke(this, e);
+        }
+
+        /// <summary>
+        /// Event that is fired when timer has elapsed
+        /// </summary>
+        public event EventHandler<TimerElapsedEventArgs> TimerElapsed;
+
+        protected virtual void OnTimerElapsed(TimerElapsedEventArgs e) {
+            EventHandler<TimerElapsedEventArgs> handler = TimerElapsed;
+            handler?.Invoke(this, e);
+        }
+    }
+
+    public class TimerElapsedEventArgs : EventArgs {
+        public string MessageFromTimer{ get; set;}
+    }
+
+    public class TimerChangedEventArgs : EventArgs {
+        public int TimerCurrentTimer { get; set; }
     }
 }
